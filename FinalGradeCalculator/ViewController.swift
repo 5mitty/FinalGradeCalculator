790e5a7107862b989a0c1dd6d83a30d8b98de0c1
Jacob Smith <jsmith0361@jh-mba158-19.d214.org>
2018-10-26 11:44:35 -0500

    calculate, not finished, debunk mainExamPart

--- FinalGradeCalculator/ViewController.swift
+++ FinalGradeCalculator/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yourGrade: UITextField!
+    @IBOutlet weak var gradeWanted: UITextField!
+    @IBOutlet weak var finalExamSize: UITextField!
+    @IBOutlet weak var examGradeNeededLabel: UILabel!
+    @IBOutlet weak var calculateButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    func calculateGrade() -> Int {
+        if let gradeWantedInt:Int = Int(gradeWanted.text!)! {
+            let yourGradeDouble:Double = Double(yourGrade.text!)!
+            let finalExamSizeInt:Int = Int(finalExamSize.text!)!
+            
+            var examGradePart:Double = Double(100 - finalExamSizeInt)
+            let finalExamSizeDouble = Double(finalExamSizeInt) / 100
+            examGradePart = examGradePart / 100
+            let examGradePartDone = examGradePart * Double(yourGradeDouble)
+            let mainGradePart:Double = finalExamSizeDouble * Double(gradeWantedInt)
+            let examGradeNeededFull:Double = Double(mainGradePart) + examGradePartDone
+            examGradeNeededLabel.text = String(examGradeNeededFull)
+            print("gradeWantedInt: " + String(gradeWantedInt))
+            print("finalExamSizeInt: " + String(finalExamSizeInt))
+            print("mainGradePart: " + String(mainGradePart))
+            print("examGradePart: " + String(examGradePart))
+            print("examGradePartDone: " + String(examGradePartDone))
+            return Int(examGradeNeededFull)
+            
+            
+        }
+        
     }
 
+    @IBAction func onCalculatePressed(_ sender: UIButton) {
+        calculateGrade()
+    }
+    
 
 }
 
