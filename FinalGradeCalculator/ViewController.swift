0a24563590f46e951153760d8fd7b19619e8b327
Jacob Smith <jsmith0361@jh-mba158-19.d214.org>
2018-10-30 11:42:36 -0500

    calculator done.

--- FinalGradeCalculator/ViewController.swift
+++ FinalGradeCalculator/ViewController.swift
@@ -26,22 +26,55 @@ class ViewController: UIViewController {
     
     func calculateGrade() -> Int {
         if let gradeWantedInt:Int = Int(gradeWanted.text!)! {
-            let yourGradeDouble:Double = Double(yourGrade.text!)!
+            var yourGradeDouble:Double = Double(yourGrade.text!)!
             let finalExamSizeInt:Int = Int(finalExamSize.text!)!
             
             var examGradePart:Double = Double(100 - finalExamSizeInt)
-            let finalExamSizeDouble = Double(finalExamSizeInt) / 100
             examGradePart = examGradePart / 100
-            let examGradePartDone = examGradePart * Double(yourGradeDouble)
-            let mainGradePart:Double = finalExamSizeDouble * Double(gradeWantedInt)
-            let examGradeNeededFull:Double = Double(mainGradePart) + examGradePartDone
-            examGradeNeededLabel.text = String(examGradeNeededFull)
-            print("gradeWantedInt: " + String(gradeWantedInt))
-            print("finalExamSizeInt: " + String(finalExamSizeInt))
-            print("mainGradePart: " + String(mainGradePart))
+            
+            let finalExamSizeDouble = Double(finalExamSizeInt) / 100
+            yourGradeDouble = yourGradeDouble / 100
+            
+            var gradeWantedDouble:Double =  Double(gradeWantedInt)
+            gradeWantedDouble = gradeWantedDouble / 100
+            
+            // new try starts here
+            
+            let wantedGradeMinusExamGradePart = examGradePart * yourGradeDouble
+            
+            let wantedGradeMinusExamTimesCurrent = gradeWantedDouble - wantedGradeMinusExamGradePart
+            
+            var wantGradeMinusExamTimesCurrentDivideExamWeight = wantedGradeMinusExamTimesCurrent / finalExamSizeDouble
+            
+            wantGradeMinusExamTimesCurrentDivideExamWeight = wantGradeMinusExamTimesCurrentDivideExamWeight * 100
+            
+            // new try ends here
+            
+            
+            //let examGradePartDone = examGradePart * Double(yourGradeDouble)
+            
+            
+            
+            //let mainGradePart:Double = finalExamSizeDouble * Double(gradeWantedInt)
+            
+            //let examGradeNeededFull:Double = Double(mainGradePart) + examGradePartDone
+            
+            //let examGradePartDone:Double = Double(mainGradePart) + Double(.8 * gradeWantedInt)
+            
+            //examGradeNeededLabel.text = String(examGradeNeededFull)
+            
+            var wantGradeMinusExamTimesCurrentDivideExamWeightInt = Int(wantGradeMinusExamTimesCurrentDivideExamWeight)
+            
+            let wantGradeMinusExamTimesCurrentDivideExamWeightIntString = String(wantGradeMinusExamTimesCurrentDivideExamWeightInt)
+            
+            examGradeNeededLabel.text = (wantGradeMinusExamTimesCurrentDivideExamWeightIntString + "%")
+            
+            
+            print("gradeWantedGradeDouble: " + String(gradeWantedDouble))
             print("examGradePart: " + String(examGradePart))
-            print("examGradePartDone: " + String(examGradePartDone))
-            return Int(examGradeNeededFull)
+            print("yourGradeDouble: " + String(yourGradeDouble))
+            print("finalExamSizeDouble: " + String(finalExamSizeDouble))
+            return Int(wantGradeMinusExamTimesCurrentDivideExamWeight)
             
             
         }
